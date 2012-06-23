954589670aef863c167d8b1e1ee991e5d52b4497
James Turk <james.p.turk@gmail.com>
2012-06-23 02:03:37 -0400

    user add now calls passwd, closes #1

--- sundrop/user.py
+++ sundrop/user.py
@@ -6,4 +6,5 @@ def add(user):
     """ create a new user with sudo rights """
     sudo('useradd {0} --base-dir /etc/skel --home-dir /home/{0} --create-home '
          '--shell /bin/bash --groups admin'.format(user))
+    sudo('passwd {0}'.format(user))
     copy_dir('_users/{0}/'.format(user), '/home/{0}/'.format(user), user)
