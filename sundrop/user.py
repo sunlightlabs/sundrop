1b062632c7577dddef0875d714c9d332e29602c4
James Turk <james.p.turk@gmail.com>
2012-06-23 02:24:36 -0400

    fix user bugs, fixes #2

--- sundrop/user.py
+++ sundrop/user.py
@@ -1,4 +1,5 @@
-from fabric.api import sudo, task
+import os
+from fabric.api import sudo, task, env
 from .utils import copy_dir
 
 @task
@@ -7,4 +8,5 @@ def add(user):
     sudo('useradd {0} --base-dir /etc/skel --home-dir /home/{0} --create-home '
          '--shell /bin/bash --groups admin'.format(user))
     sudo('passwd {0}'.format(user))
-    copy_dir('_users/{0}/'.format(user), '/home/{0}/'.format(user), user)
+    copy_dir(os.path.join(env.CONFIG_DIR, '_users/{0}/'.format(user)),
+             '/home/{0}/'.format(user), user)
