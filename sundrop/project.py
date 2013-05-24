6d8428954d47bc9af40f44672b48e9c17eae7401
James Turk <james.p.turk@gmail.com>
2013-05-24 12:19:40 -0400

    collectstatic

--- sundrop/project.py
+++ sundrop/project.py
@@ -243,4 +243,11 @@ def tail(logname):
 
 @task
 def django(command):
-    sudo('/projects/{projname}/virt/bin/python `find /projects/{projname}/src/ -name manage.py` {0}'.format(command, **env))
+    command = '/projects/{projname}/virt/bin/python `find /projects/{projname}/src/ -name manage.py` {0}'.format(command, **env)
+    if env.server_type == 'staging' and 'staging_settings' in env.proj:
+        command += ' --settings={0}'.format(env.proj['staging_settings'])
+    sudo(command)
+
+@task
+def collectstatic():
+    django('collectstatic')
